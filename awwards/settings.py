2fb4d5734878a16a856ce247b4549e520a6ef003
Evohmike <evohmik@gmail.com>
2018-10-12 15:20:58 +0300

    added app and bootstrap in the setting

--- awwards/settings.py
+++ awwards/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
