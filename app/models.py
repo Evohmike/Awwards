09f1f43e30d42acd58825923eb0652fbaf855ef8
Evohmike <evohmik@gmail.com>
2018-10-12 15:20:33 +0300

    created prifile class in the model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+  first_name = models.CharField(max_length=40)
+  last_name = models.CharField(max_length=30)
+  profile_picture = models.ImageField(upload_to='users/')
+  bio = models.TextField(default="I love Awward!")
+  pub_date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
