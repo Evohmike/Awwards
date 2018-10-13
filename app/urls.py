dd3e827860e48e1de1c170646f96c7c49b994c9a
Evohmike <evohmik@gmail.com>
2018-10-13 10:27:17 +0300

    changed url

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,3 @@
-
 from django.conf.urls import url
 from . import views
 from django.conf import settings
@@ -8,4 +7,4 @@ urlpatterns=[
     url('^$',views.home,name = 'home'),
 ]
 if settings.DEBUG:
-  urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
