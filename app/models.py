a1edbd5baaa8c323a4c5584896a9c10ad36837fd
Evohmike <evohmik@gmail.com>
2018-10-14 20:51:00 +0300

    created class profile

--- app/models.py
+++ app/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 # Create your models here.
 
@@ -16,3 +29,16 @@ class Profile(models.Model):
         return self.first_name
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User,null=True,related_name='profile')
+    name = models.CharField(max_length=50)
+    profile_photo = models.ImageField(upload_to='images/', blank=True,)
+    user_name = models.CharField(max_length=50, null=True)
+    occupation = models.CharField(max_length=300, null=True)
+    bio = models.TextField(blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return self.first_name
+
+    
