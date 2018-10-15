dc445f2f797639ec68925152400ae90b8204b6c4
Evohmike <evohmik@gmail.com>
2018-10-15 22:45:55 +0300

    added url for project

--- app/urls.py
+++ app/urls.py
@@ -10,6 +10,8 @@ urlpatterns=[
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     url(r'^new/image$', views.new_image, name='new-image'),
+    url(r'^project/$',views.project,name ='project'),
+
 
 ]
 if settings.DEBUG:
